2539c8636cce60bc6b001a99e9bc7f8fab559c6e
betterclient <103489620+betterclient@users.noreply.github.com>
2022-10-27 19:45:06 +0300

    I WASNT SUPPOSED TO COMMIT THAT

--- src/main/java/tk/simplexclient/utils/Util.java
+++ src/main/java/tk/simplexclient/utils/Util.java
@@ -2,6 +2,6 @@ package tk.simplexclient.utils;
 
 public class Util {
     public static boolean isDevEnv(){
-        return !Boolean.getBoolean("fabric.development");
+        return Boolean.getBoolean("fabric.development");
     }
 }
